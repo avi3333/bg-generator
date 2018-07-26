83dfc56c746d2d1ec74d5fa173175a5cae47195b
avi3333 <40758004+avi3333@users.noreply.github.com>
2018-07-26 16:06:01 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 let css = document.querySelector('h3');
-let para = document.querySelector('p')
 let color1= document.querySelector('#color1');
 let color2= document.querySelector('#color2');
 let body= document.querySelector('#gradient');
