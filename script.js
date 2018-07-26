986adfbdd28a7d38d6935243031d083a5663af70
Avi <avijit.chy019@gmail.com>
2018-07-26 12:15:43 +0600

    Changed some text

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let css = document.querySelector('h3');
+let para = document.querySelector('p')
 let color1= document.querySelector('#color1');
 let color2= document.querySelector('#color2');
 let body= document.querySelector('#gradient')
@@ -6,6 +7,7 @@ let body= document.querySelector('#gradient')
 const setGradient = () => {
      body.style.background = 'linear-gradient(to right, ' + color1.value + ', ' + color2.value + ')';
      css.textContent= body.style.background + ';'
+     para.textContent= 'Copy and paste this code in your CSS';
 }
 
 color1.addEventListener('input', setGradient);
